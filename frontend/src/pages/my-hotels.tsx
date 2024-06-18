578f66bb0ec4139f6e8c19db92344bb29345d670
Dival Sehgal <sehgaldival@gmail.com>
2024-06-18 21:14:39 +0530

    refactor

--- frontend/src/pages/my-hotels.tsx
+++ frontend/src/pages/my-hotels.tsx
@@ -38,6 +38,7 @@ const MyHotels = () => {
           return (
             <div
               data-testid="hotel-card"
+              key={hotel._id}
               className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 gap-5"
             >
               <h2 className="text-2xl font-bold">{hotel.name}</h2>
